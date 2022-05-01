430b69150f796ad0c17d32a9c84295af920cb8c1
Tolga Özen <mtolgaozen@gmail.com>
2022-05-01 12:45:39 +0300

    Update resource.go

--- resource.go
+++ resource.go
@@ -4,13 +4,15 @@ type Resource struct {
 	ID         string                 `json:"id"`
 	Type       string                 `json:"type"`
 	Attributes map[string]interface{} `json:"attributes"`
+	Group      interface{}            `json:"group"`
 }
 
 // NewResource .
-func NewResource(id string, typ string, attributes map[string]interface{}) Resource {
+func NewResource(id string, typ string, attributes map[string]interface{}, group interface{}) Resource {
 	return Resource{
 		ID:         id,
 		Type:       typ,
 		Attributes: attributes,
+		Group:      group,
 	}
 }
